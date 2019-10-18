a2e04be4e05764f8ec02743731c845f0ca9d882c
bxker <jake.luke.baker@gmail.com>
2019-10-18 16:45:08 -0500

    added redux store and user reducer files. Also added register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
 
 export default class Register extends Component {
+    constructor(){
+        super()
+        this.state = {
+            first_name: "",
+            last_name: "",
+            username: "",
+            password: "",
+            email: "",
+            phone_number: 12223334444
+        }
+    }
+
+
     render() {
         return (
-            <div>
+            <form>
                 <h1>Register</h1>
-            </div>
+                <label>First Name</label>
+                <input ></input>
+                <label>Last Name</label>
+                <input ></input>
+                <label>Username</label>
+                <input ></input>
+                <label>Password</label>
+                <input ></input>
+                <label>Email</label>
+                <input ></input>
+                <label>Phone Number</label>
+                <input ></input>
+                <button>Register</button>
+            </form>
         )
     }
 }
