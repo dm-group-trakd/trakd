fd31a5f25d8023e8652b4719232e3eee484636b2
bxker <jake.luke.baker@gmail.com>
2019-10-21 12:07:08 -0500

    added conditional rendering using the location.pathname from withRouter to allow the register comp to render

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './src-styles/App/App.scss';
 import {connect} from 'react-redux';
 import {getSession} from './redux/reducers/userReducer';
 import routes from './routes';
+import {withRouter} from 'react-router-dom';
 
 
 //components
 import Nav from '../src/components/Nav/Nav';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 
 
 class App extends Component{
+
+
   render(){
     return (
       <div className="App">
         {!this.props.user_id 
         ?
-        <Login />
+        this.props.location.pathname === "/" ?
+          <Login />
+        :
+        <Register />
         :
         <>
           <Nav/>
@@ -36,6 +43,6 @@ const mapStateToProps = reduxState => {
 }
 
 
-export default connect(mapStateToProps, {
+export default withRouter(connect(mapStateToProps, {
   getSession
-})(App)
+})(App))
