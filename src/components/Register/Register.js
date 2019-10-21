6a79de68aa99d364ebb7240125b62b2d5c947182
kevin <kevin.hernandez1802@gmail.com>
2019-10-21 13:35:32 -0500

    finish register style

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,7 +6,7 @@ import TextField from '@material-ui/core/TextField';
 import { Redirect, Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {registerUser, getSession} from '../../redux/reducers/userReducer';
-import './styles/Register.css';
+import './styles/Register.scss';
 
 class Register extends Component {
     constructor(){
@@ -40,95 +40,103 @@ class Register extends Component {
             return <Redirect to="/add"/>
         }
         return (
-            
-            <Card>
-                <CardContent>
-                    <section>
-                        {/* <img>Logo</img>
-                         */}
-                        <h1>Logo</h1>
-                        <h1>Sign Up</h1>
-                    </section>
-                    <section>
-                        <TextField
-                            id="outlined-first_name-input"
-                            label="First Name"
-                            type="text"
-                            name="first_name"
-                            autoComplete="first_name"
-                            margin="normal"
-                            variant="outlined"
-                            onChange={this.handleInput}
-                            required
-                        ></TextField>
-                        <TextField
-                            id="outlined-last_name-input"
-                            label="Last Name"
-                            type="text"
-                            name="last_name"
-                            autoComplete="last_name"
-                            margin="normal"
-                            variant="outlined"
-                            onChange={this.handleInput}
-                            required
-                        ></TextField>
-                        <TextField
-                            id="outlined-username-input"
-                            label="Username"
-                            type="text"
-                            name="username"
-                            autoComplete="username"
-                            margin="normal"
-                            variant="outlined"
-                            onChange={this.handleInput}
-                            required
-                        ></TextField>
-                    </section>
-                    <section>
-                        <TextField
-                            id="outlined-password-input"
-                            label="Password"
-                            type="password"
-                            name="password"
-                            autoComplete="password"
-                            margin="normal"
-                            variant="outlined"
-                            onChange={this.handleInput}
-                            required
-                        ></TextField>
-                        <TextField
-                            id="outlined-email-input"
-                            label="Email"
-                            type="email"
-                            name="email"
-                            autoComplete="email"
-                            margin="normal"
-                            variant="outlined"
-                            onChange={this.handleInput}
-                            required
-                        ></TextField>
-                        <TextField
-                            id="outlined-phone_number-input"
-                            label="Phone Number"
-                            type="phone"
-                            name="phone_number"
-                            autoComplete="phone_number"
-                            margin="normal"
-                            variant="outlined"
-                            onChange={this.handleInput}
-                            required
-                        ></TextField>
-                    </section>
-                    <section>
-                            <Button
-                                variant="contained" 
-                                color="primary"
-                                onClick={this.handleSubmit}
-                            >Register</Button>
-                        <h1>Already have an account? Login <Link to="/">here!</Link></h1>
-                    </section>
-                </CardContent>
-            </Card>
+            <div className = "register-container">
+                <Card>
+                    <div className = "card-container">
+                        <CardContent>
+                            <div className = "card-content">
+                            <section className = "head">
+                                {/* <img>Logo</img>
+                                */}
+                                <img  className = "img" src = 'https://files.slack.com/files-pri/T039C2PUY-FP8DJ32Q2/cover.png' alt =""/>
+                                <h1 className ="signUp">Sign Up</h1>
+                            </section>
+                            <div className = "inputs">
+                            <section className = "first-row">
+                                <TextField
+                                    id="outlined-first_name-input"
+                                    label="First Name"
+                                    type="text"
+                                    name="first_name"
+                                    autoComplete="first_name"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={this.handleInput}
+                                    required
+                                ></TextField>
+                                <TextField
+                                    id="outlined-last_name-input"
+                                    label="Last Name"
+                                    type="text"
+                                    name="last_name"
+                                    autoComplete="last_name"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={this.handleInput}
+                                    required
+                                ></TextField>
+                                <TextField
+                                    id="outlined-username-input"
+                                    label="Username"
+                                    type="text"
+                                    name="username"
+                                    autoComplete="username"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={this.handleInput}
+                                    required
+                                ></TextField>
+                            </section>
+                            <section className = "second-row">
+                                <TextField
+                                    id="outlined-password-input"
+                                    label="Password"
+                                    type="password"
+                                    name="password"
+                                    autoComplete="password"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={this.handleInput}
+                                    required
+                                ></TextField>
+                                <TextField
+                                    id="outlined-email-input"
+                                    label="Email"
+                                    type="email"
+                                    name="email"
+                                    autoComplete="email"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={this.handleInput}
+                                    required
+                                ></TextField>
+                                <TextField
+                                    id="outlined-phone_number-input"
+                                    label="Phone Number"
+                                    type="phone"
+                                    name="phone_number"
+                                    autoComplete="phone_number"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={this.handleInput}
+                                    required
+                                ></TextField>
+                            </section>
+                            </div>
+                            <section className = 'register'>
+                                    <Button
+                                        className ="reg-btn"
+                                        variant="contained" 
+                                        color="primary"
+                                        onClick={this.handleSubmit}
+                                    >Register</Button>
+                                <h1>Already have an account? Login <Link to="/">here!</Link></h1>
+                            </section>
+                            </div>
+                        </CardContent>
+                        </div>
+                 </Card>
+            </div>
         )
     }
 }
