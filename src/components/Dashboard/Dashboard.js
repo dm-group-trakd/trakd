f51a29b72713f796ce3477c9f1ba71217c675c33
unknown <jensenjamison@yahoo.com>
2019-10-23 11:44:19 -0500

    added food component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,20 +1,26 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Calories from "../calories/Calories"
+import Food from "../Food/Food"
+import "./styles/Dashboard.css"
 
-
-export default class Dashboard extends Component{
-    constructor(){
+export default class Dashboard extends Component {
+    constructor() {
         super();
-        this.state={
+        this.state = {
 
         }
 
     }
-    render(){
-        return(
-            <div>
+    render() {
+        return (
+            <div className="dashboard-container">
                 {/* <h1>Dashboard</h1> */}
-                <Calories/>
+                <div>
+                    <Calories />
+                </div>
+                <div>
+                    <Food />
+                </div>
             </div>
         )
     }
