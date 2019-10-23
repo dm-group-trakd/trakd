5bb49315e7b9cb2ba098aa46492418b9a6577f51
Freddy Ernst <freddyernst@Freddys-MacBook-Pro.local>
2019-10-23 13:40:51 -0500

    Started the chart component

--- src/components/calories/Calories.js
+++ src/components/calories/Calories.js
@@ -2,27 +2,41 @@ import React from 'react'
 import './styles/calories.scss'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import {Doughnut} from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
 
-class Calories extends React.Component{
-    constructor(){
+class Calories extends React.Component {
+    constructor() {
         super()
+        this.state = {
+            eaten: 0,
+            eatenHolder: 0
+        }
     }
-    render(){
+
+    setEaten = value => {
+        this.setState({ eatenHolder: +value})
+    }
+
+    setGoal = () => {
+        this.setState({
+            eaten: this.state.eaten + this.state.eatenHolder
+        })
+    }
+    render() {
         const data = {
             labels: ["", "calories"],
             datasets: [
-              {
-                data: [1000, 2000],
-                backgroundColor: ["#36A2EB", "#dae0e6"]
-              }
+                {
+                    data: [this.state.eaten, 2000 - this.state.eaten],
+                    backgroundColor: ["#36A2EB", "#dae0e6"]
+                }
             ]
-          };
-        return(
-            <div className = 'calorie-container'>
+        };
+        return (
+            <div className='calorie-container'>
                 <Card>
                     <CardContent>
-                        <Doughnut data={data} width ="700" height ="600" />
+                        <Doughnut data={data} width="700" height="600" />
                     </CardContent>
                 </Card>
             </div>
