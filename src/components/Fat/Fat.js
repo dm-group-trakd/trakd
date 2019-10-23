d40b478737929a27abbf357313f7020e56f55c93
unknown <jensenjamison@yahoo.com>
2019-10-23 15:22:46 -0500

    added fat component

---
+++ src/components/Fat/Fat.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import './styles/calories.scss'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { Doughnut } from 'react-chartjs-2'
+import {connect} from 'react-redux'
+import {getFood} from '../../redux/reducers/foodReducer'
+
+class Fat extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            eaten: 0
+        }
+    }
+
+    componentDidMount = () => {
+        this.props.getFood().then(() => {
+
+            let fatTotal = 0
+            for (let i = 0; i < this.props.food.length; i++) {
+
+                fatTotal += this.props.food[i].fat
+                
+            }
+            this.setState({
+                
+            
+                    eaten: fatTotal
+                
+            })
+        })
+    
+        console.log(this.props.food)
+    }
+    // setEaten = value => {
+    //     this.setState({ eatenHolder: +value})
+    // }
+
+    // setGoal = () => {
+    //     this.setState({
+    //         eaten: this.state.eaten + this.state.eatenHolder
+    //     })
+    // }
+    render() {
+        const data = {
+            labels: ["", "fat"],
+            datasets: [
+                {
+                    data: [this.state.eaten, 2000 - this.state.eaten ],
+                    backgroundColor: ["#36A2EB", "#dae0e6"]
+                }
+            ]
+        };
+        return (
+            <div className='calorie-container'>
+                <Card>
+                    <CardContent>
+                        <Doughnut data={data} width="700" height="600" />
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+}
+const mapStateToProps = reduxState => {
+    return {
+        food: reduxState.foodReducer.food
+    }
+}
+export default connect(mapStateToProps, {
+getFood
+})(Fat);
