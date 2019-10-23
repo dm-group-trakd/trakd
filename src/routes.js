eb31c3d83fb1a07520c1917d14a99fe5375a0252
Freddy Ernst <freddyernst@Freddys-MacBook-Pro.local>
2019-10-23 09:17:27 -0500

    Added further styling to about component

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,14 @@ import AddNutrition from "./components/AddNutrition/AddNutrition";
 
 
 
+
 export default(
     <Switch>
         <Route exact path="/" component={Login}/>
         <Route path="/register" component={Register} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/add" component={AddNutrition} />
+        
     
         <Route render={() => {
             return <h1>404 Not Found</h1>
