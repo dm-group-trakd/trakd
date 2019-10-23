f2a93ccb9c5d94ccbc3538905333ea45ab9508b8
Freddy Ernst <freddyernst@Freddys-MacBook-Pro.local>
2019-10-23 15:08:06 -0500

    Calorie chart functional

--- src/components/calories/Calories.js
+++ src/components/calories/Calories.js
@@ -3,31 +3,51 @@ import './styles/calories.scss'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Doughnut } from 'react-chartjs-2'
+import {connect} from 'react-redux'
+import {getFood} from '../../redux/reducers/foodReducer'
 
 class Calories extends React.Component {
     constructor() {
         super()
         this.state = {
-            eaten: 0,
-            eatenHolder: 0
+            eaten: 0
         }
     }
 
-    setEaten = value => {
-        this.setState({ eatenHolder: +value})
-    }
+    componentDidMount = () => {
+        this.props.getFood().then(() => {
 
-    setGoal = () => {
-        this.setState({
-            eaten: this.state.eaten + this.state.eatenHolder
+            let calorieTotal = 0
+            for (let i = 0; i < this.props.food.length; i++) {
+
+                calorieTotal += this.props.food[i].calories
+                
+            }
+            this.setState({
+                
+            
+                    eaten: calorieTotal
+                
+            })
         })
+    
+        console.log(this.props.food)
     }
+    // setEaten = value => {
+    //     this.setState({ eatenHolder: +value})
+    // }
+
+    // setGoal = () => {
+    //     this.setState({
+    //         eaten: this.state.eaten + this.state.eatenHolder
+    //     })
+    // }
     render() {
         const data = {
             labels: ["", "calories"],
             datasets: [
                 {
-                    data: [this.state.eaten, 2000 - this.state.eaten],
+                    data: [this.state.eaten, 2000 - this.state.eaten ],
                     backgroundColor: ["#36A2EB", "#dae0e6"]
                 }
             ]
@@ -43,4 +63,11 @@ class Calories extends React.Component {
         )
     }
 }
-export default Calories;
+const mapStateToProps = reduxState => {
+    return {
+        food: reduxState.foodReducer.food
+    }
+}
+export default connect(mapStateToProps, {
+getFood
+})(Calories);
